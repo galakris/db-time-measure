ec052acd7de283b5c4e8f3f2480b54f57d0e7f27
Krzysztof Galuszka <krzysztof.galuszka95@gmail.com>
2021-07-29 19:41:43 +0200

    DbConnection and QueryMeasure

--- src/main/java/DBQueryMeasure.java
+++ src/main/java/DBQueryMeasure.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class DBQueryMeasure {
+import java.sql.*;
+import java.time.Duration;
+import java.time.Instant;
+
+public class DBQueryMeasure {
+
+
+    public void measureQueryTime(String query, int amount) {
+
+        try {
+            Connection myConnection = DbConnection.getConnection();
+
+            Statement myStatement = myConnection.createStatement();
+
+            for (int i = 0; i < amount; i++) {
+                Instant start = Instant.now();
+                ResultSet resultSet = myStatement.executeQuery(query);
+                Instant finish = Instant.now();
+                long timeElapsed = Duration.between(start, finish).toMillis();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- src/main/java/DbConnection.java
+++ src/main/java/DbConnection.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class DbConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    private static Connection connection = null;
+    static String CONNECTION_URL  = "jdbc:mysql://localhost:3306/creation_test?useSSL=false&serverTimezone=UTC";
+    static String CONNECTION_USERNAME = "root";
+    static String CONNECTION_PASSWORD = "Galakris95$";
+
+    public static Connection getConnection(){
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(CONNECTION_URL, CONNECTION_USERNAME, CONNECTION_PASSWORD);
+            } catch (SQLException e) {
+                System.out.println("Unable to connect to database.");
+                e.printStackTrace();
+                return null;
+            }
+            return connection;
+        } else {
+            return connection;
+        }
+    }
 }
